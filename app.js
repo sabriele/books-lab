c9227863fad60f2ec74eeec8283dae8734ac23f5
Sabrina Tjeng <sabriele@users.noreply.github.com>
2019-03-14 12:21:07 +0800

    Add a favicon for the site

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const books = require("./routes/books");
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send("Welcome to The Bookstore");
 });
