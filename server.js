7da09cc8089d89001859821084a74b4688e66942
Sabrina Tjeng <sabriele@users.noreply.github.com>
2019-03-20 10:52:12 +0800

    Create and test full CRUD with mongoose

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ const app = require("./app");
 const port = process.env.PORT || 8080;
 const isInProduction = process.env.NODE_ENV === "production";
 
-app.listen(port, () => {
-  if (isInProduction) {
-    console.log(`App is now running on Heroku with port number ${port}`);
-  } else {
-    console.log(`App is now running on http://localhost:${port}`);
-  }
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/bookstore", { useNewUrlParser: true });
+
+const db = mongoose.connection;
+
+db.on("error", err => {
+  console.error(console, "connection error:", err);
+});
+db.once("open", () => {
+  console.log("Succesfully connected to the database.");
+  app.listen(port, () => {
+    if (isInProduction) {
+      console.log(`App is now running on Heroku with port number ${port}`);
+    } else {
+      console.log(`App is now running on http://localhost:${port}`);
+    }
+  });
 });
